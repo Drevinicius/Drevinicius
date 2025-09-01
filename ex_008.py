d0f67d0f8ae37670f5e80ca3d6aa3353c1e59fb7
André Vinícius Reis de Souza Magno <166178954+Drevinicius@users.noreply.github.com>
2025-09-01 12:58:16 -0400

    Delete ex_008.py

--- ex_008.py
+++
@@ -1,9 +0,0 @@
-"""Somando intervalo de numeros N"""
-
-def soma (numero):
-    contador = 0
-    for valor in range(1, numero+1):
-        contador += valor
-    return contador
-
-print(f"Soma: {soma(5)}") # Saída: 15
