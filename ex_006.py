62a3c4e1f408fc64fdf25e7a994d286eb1f1622a
André Vinícius Reis de Souza Magno <166178954+Drevinicius@users.noreply.github.com>
2025-09-01 12:57:41 -0400

    Delete ex_006.py

--- ex_006.py
+++
@@ -1,6 +0,0 @@
-"""Identificador de palindromos"""
-
-texto = "radar"
-texto_inverso = texto[::-1] # Recebe o texto inverso
-
-print("É palindromo" if texto_inverso == texto else "Não é palindromo") # Saída: É palindromo
