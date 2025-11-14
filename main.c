f1e5f28e8998c315643f72965d09ab114d512227
André Vinícius Reis de Souza Magno <166178954+Drevinicius@users.noreply.github.com>
2025-11-14 10:18:48 -0400

    Exemplo de alocação de memoria com lista encadeada simples e verificação de endereços na memoria

---
+++ main.c
@@ -0,0 +1,50 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct no{
+    int valor;
+    struct no *prox;
+} No;
+
+void exibir(No *lista){
+    No *aux = lista;
+    while(aux){
+        printf("\tEndereco: %p\nValor: %d\nProximo: %p\n\n", (void *)aux, aux->valor, aux->prox);
+        aux = aux->prox;
+    }
+}
+
+void criarNo(No **lista, int valor){
+    No *novoNo = (No *)malloc(sizeof(No));
+    if(!novoNo){
+        *lista = NULL;
+        return;
+    }
+    novoNo->valor = valor;
+    novoNo->prox = NULL;
+    *lista = novoNo;
+}
+
+void inserirInicio(No **lista, int valor){
+    No *novoNo;
+    criarNo(&novoNo, valor);
+    if(!(*lista)){
+        *lista = novoNo;
+        return;
+    }
+    novoNo->prox = *lista;
+    *lista = novoNo;
+}
+
+int main() {
+    No *lista = NULL;
+    inserirInicio(&lista, 4);
+    inserirInicio(&lista, 3);
+    inserirInicio(&lista, 2);
+    inserirInicio(&lista, 1);
+
+    exibir(lista);
+    
+    return 0;
+}
